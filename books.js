2173f2579f4445a50eb62c3765d411c7cc465f13
Cathy Dao <cathydao95@gmail.com>
2023-08-22 17:44:24 -0700

    update JSON formatting

--- books.js
+++ books.js
@@ -1,51 +1,58 @@
 const BOOKS = [
   {
-    title: "Where the Sidewalk Ends",
-    author: "Shel Silverstein",
-    image_url: "https://example.com/where_the_sidewalk_ends.jpg",
-    summary:
+    "id": "1",
+    "title": "Where the Sidewalk Ends",
+    "author": "Shel Silverstein",
+    "image_url": "https://example.com/where_the_sidewalk_ends.jpg",
+    "summary":
       "A collection of whimsical and imaginative poems that explore the joy and wonder of childhood, taking readers on a journey to fantastical places.",
   },
   {
-    title: "The Cat in the Hat",
-    author: "Dr. Seuss",
-    image_url: "https://example.com/the_cat_in_the_hat.jpg",
-    summary:
+    "id": "2",
+    "title": "The Cat in the Hat",
+    "author": "Dr. Seuss",
+    "image_url": "https://example.com/the_cat_in_the_hat.jpg",
+    "summary":
       "Two children's rainy day is turned upside down by the mischievous Cat in the Hat and his zany antics, bringing chaos and laughter to their home.",
   },
   {
-    title: "Diary of a Wimpy Kid",
-    author: "Jeff Kinney",
-    image_url: "https://example.com/diary_of_a_wimpy_kid.jpg",
-    summary:
+    "id": "3",
+    "title": "Diary of a Wimpy Kid",
+    "author": "Jeff Kinney",
+    "image_url": "https://example.com/diary_of_a_wimpy_kid.jpg",
+    "summary":
       "Join Greg Heffley as he navigates the ups and downs of middle school life, chronicling his hilarious and relatable experiences in his diary.",
   },
   {
-    title: "Pippi Longstocking",
-    author: "Astrid Lindgren",
-    image_url: "https://example.com/pippi_longstocking.jpg",
-    summary:
+    "id": "4",
+    "title": "Pippi Longstocking",
+    "author": "Astrid Lindgren",
+    "image_url": "https://example.com/pippi_longstocking.jpg",
+    "summary":
       "Meet Pippi Longstocking, a strong and independent girl with a flair for adventure and a heart of gold, as she embarks on playful escapades.",
   },
   {
-    title: "Brown Bear, Brown Bear, What Do You See?",
-    author: "Bill Martin Jr.",
-    image_url: "https://example.com/brown_bear_brown_bear.jpg",
-    summary:
+    "id": "5",
+    "title": "Brown Bear, Brown Bear, What Do You See?",
+    "author": "Bill Martin Jr.",
+    "image_url": "https://example.com/brown_bear_brown_bear.jpg",
+    "summary":
       "A colorful and rhythmic picture book that introduces young readers to a variety of animals and colors through a repetitive and engaging narrative.",
   },
   {
-    title: "Corduroy",
-    author: "Don Freeman",
-    image_url: "https://example.com/corduroy.jpg",
-    summary:
+    "id": "6",
+    "title": "Corduroy",
+    "author": "Don Freeman",
+    "image_url": "https://example.com/corduroy.jpg",
+    "summary":
       "Follow the heartwarming journey of Corduroy, a teddy bear in a department store, as he searches for his missing button and finds a new friend.",
   },
   {
-    title: "Green Eggs and Ham",
-    author: "Dr. Seuss",
-    image_url: "https://example.com/green_eggs_and_ham.jpg",
-    summary:
+    "id": "7",
+    "title": "Green Eggs and Ham",
+    "author": "Dr. Seuss",
+    "image_url": "https://example.com/green_eggs_and_ham.jpg",
+    "summary":
       "Sam-I-Am tries to convince a skeptical character to try green eggs and ham in various locations and circumstances, leading to surprising results.",
   },
 ];
