ccc6ac7673af6366ac932bc247c0069a7943c3f4
Cathy Dao <cathydao95@gmail.com>
2023-08-25 12:26:44 -0700

    add images

--- books.js
+++ books.js
@@ -3,7 +3,7 @@ const BOOKS = [
     id: "1",
     title: "Where the Sidewalk Ends",
     author: "Shel Silverstein",
-    img: "https://example.com/where_the_sidewalk_ends.jpg",
+    img: "https://othergoose.com/wp-content/uploads/2019/06/where-the-sidewalk-ends.jpg",
     summary:
       "A collection of whimsical and imaginative poems that explore the joy and wonder of childhood, taking readers on a journey to fantastical places.",
   },
@@ -11,7 +11,7 @@ const BOOKS = [
     id: "2",
     title: "The Cat in the Hat",
     author: "Dr. Seuss",
-    img: "https://example.com/the_cat_in_the_hat.jpg",
+    img: "https://dwcp78yw3i6ob.cloudfront.net/wp-content/uploads/2016/12/09134440/CatintheHat.jpg",
     summary:
       "Two children's rainy day is turned upside down by the mischievous Cat in the Hat and his zany antics, bringing chaos and laughter to their home.",
   },
@@ -19,7 +19,7 @@ const BOOKS = [
     id: "3",
     title: "Diary of a Wimpy Kid",
     author: "Jeff Kinney",
-    img: "https://example.com/diary_of_a_wimpy_kid.jpg",
+    img: "https://cdn.kobo.com/book-images/df9f40fe-4fac-4799-b8e9-0449c3717c80/1200/1200/False/diario-de-greg-english-learner-s-edition-1-diary-of-a-wimpy-kid.jpg",
     summary:
       "Join Greg Heffley as he navigates the ups and downs of middle school life, chronicling his hilarious and relatable experiences in his diary.",
   },
@@ -27,7 +27,7 @@ const BOOKS = [
     id: "4",
     title: "Pippi Longstocking",
     author: "Astrid Lindgren",
-    img: "https://example.com/pippi_longstocking.jpg",
+    img: "https://cdn.kobo.com/book-images/925d0031-f1f6-470c-984c-a76744ddcd45/353/569/90/False/do-you-know-pippi-longstocking.jpg",
     summary:
       "Meet Pippi Longstocking, a strong and independent girl with a flair for adventure and a heart of gold, as she embarks on playful escapades.",
   },
@@ -35,7 +35,7 @@ const BOOKS = [
     id: "5",
     title: "Brown Bear, Brown Bear, What Do You See?",
     author: "Bill Martin Jr.",
-    img: "https://example.com/brown_bear_brown_bear.jpg",
+    img: "https://m.media-amazon.com/images/I/81Ukrj-NTgL._AC_UF1000,1000_QL80_.jpg",
     summary:
       "A colorful and rhythmic picture book that introduces young readers to a variety of animals and colors through a repetitive and engaging narrative.",
   },
@@ -43,7 +43,7 @@ const BOOKS = [
     id: "6",
     title: "Corduroy",
     author: "Don Freeman",
-    img: "https://example.com/corduroy.jpg",
+    img: "https://library-of-congress-shop.myshopify.com/cdn/shop/products/corduroy_grande.jpg?v=1617405253",
     summary:
       "Follow the heartwarming journey of Corduroy, a teddy bear in a department store, as he searches for his missing button and finds a new friend.",
   },
@@ -51,7 +51,7 @@ const BOOKS = [
     id: "7",
     title: "Green Eggs and Ham",
     author: "Dr. Seuss",
-    img: "https://example.com/green_eggs_and_ham.jpg",
+    img: "https://upload.wikimedia.org/wikipedia/en/1/11/Green_Eggs_and_Ham.jpg",
     summary:
       "Sam-I-Am tries to convince a skeptical character to try green eggs and ham in various locations and circumstances, leading to surprising results.",
   },
