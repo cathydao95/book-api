95eb2eb10dc2565fca8e1e1f15d1d99d264a87e1
Cathy Dao <cathydao95@gmail.com>
2023-08-24 21:30:23 -0700

    update json

--- books.js
+++ books.js
@@ -1,5 +1,6 @@
 const BOOKS = [
   {
+    id: "1",
     title: "Where the Sidewalk Ends",
     author: "Shel Silverstein",
     img: "https://example.com/where_the_sidewalk_ends.jpg",
@@ -7,6 +8,7 @@ const BOOKS = [
       "A collection of whimsical and imaginative poems that explore the joy and wonder of childhood, taking readers on a journey to fantastical places.",
   },
   {
+    id: "2",
     title: "The Cat in the Hat",
     author: "Dr. Seuss",
     img: "https://example.com/the_cat_in_the_hat.jpg",
@@ -14,6 +16,7 @@ const BOOKS = [
       "Two children's rainy day is turned upside down by the mischievous Cat in the Hat and his zany antics, bringing chaos and laughter to their home.",
   },
   {
+    id: "3",
     title: "Diary of a Wimpy Kid",
     author: "Jeff Kinney",
     img: "https://example.com/diary_of_a_wimpy_kid.jpg",
@@ -21,6 +24,7 @@ const BOOKS = [
       "Join Greg Heffley as he navigates the ups and downs of middle school life, chronicling his hilarious and relatable experiences in his diary.",
   },
   {
+    id: "4",
     title: "Pippi Longstocking",
     author: "Astrid Lindgren",
     img: "https://example.com/pippi_longstocking.jpg",
@@ -28,6 +32,7 @@ const BOOKS = [
       "Meet Pippi Longstocking, a strong and independent girl with a flair for adventure and a heart of gold, as she embarks on playful escapades.",
   },
   {
+    id: "5",
     title: "Brown Bear, Brown Bear, What Do You See?",
     author: "Bill Martin Jr.",
     img: "https://example.com/brown_bear_brown_bear.jpg",
@@ -35,6 +40,7 @@ const BOOKS = [
       "A colorful and rhythmic picture book that introduces young readers to a variety of animals and colors through a repetitive and engaging narrative.",
   },
   {
+    id: "6",
     title: "Corduroy",
     author: "Don Freeman",
     img: "https://example.com/corduroy.jpg",
@@ -42,6 +48,7 @@ const BOOKS = [
       "Follow the heartwarming journey of Corduroy, a teddy bear in a department store, as he searches for his missing button and finds a new friend.",
   },
   {
+    id: "7",
     title: "Green Eggs and Ham",
     author: "Dr. Seuss",
     img: "https://example.com/green_eggs_and_ham.jpg",
